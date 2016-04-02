2cf8bbf45820ce9b88209fc7114d5ce74dd34d20
jhaocheng <jhaocheng.wu@gmail.com>
2016-04-02 22:59:45 +0800

    no jhao

--- js/index.js
+++ js/index.js
@@ -4,31 +4,9 @@ $( document ).ready(function(){
 	},500);
 	//clear svg region
 	setTimeout(function(){
-		$('.name-show').show();
 		$('#svg-region').fadeOut(function(){
-			$('#jhao').fadeTo('slow', 0.9, function(){
-				setTimeout(function(){
-					$('#yang').fadeTo('slow', 0.9, function(){
-						$('#yang').fadeTo(1200, 0, function(){
-							$('#yang').css('display','none');
-							$('#chi').css('display','initial');
-							$('#chi').fadeTo('slow', 0.9, function(){
-								$('#chi').fadeTo(1200, 0, function(){
-									$('#chi').css('display','none');
-									$('#cheng').css('display','initial');
-									$('#cheng').fadeTo('slow', 0.9);
-									setTimeout(function(){
-										$('.name-show').fadeOut(700).fadeIn(700).fadeOut(500).fadeIn(500).fadeOut(400).fadeIn(400).fadeOut(300).fadeIn(300).fadeOut(300,function(){
-											$('.cv').fadeIn(1000);
-										});
-									},1000)
-								});
-							});
-						});
-					});
-				},600);
-			});
+			$('.cv').fadeIn(1000);
 		});
-	},12000)
+	},11000)
 
 })

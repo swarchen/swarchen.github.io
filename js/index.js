f0c838ef6cd3cb1c729a651d649ada2e95fd7339
jhaocheng <jhaocheng.wu@gmail.com>
2016-04-02 23:13:08 +0800

    try opacity svg

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 $( document ).ready(function(){
-	setTimeout(function(){
-		$('#svg-region').show();
-	},500);
+	
 	//clear svg region
 	setTimeout(function(){
 		$('#svg-region').fadeOut(function(){

a96c0881f171a827cbdccd04867a8fee02572ab5
jhaocheng <jhaocheng.wu@gmail.com>
2016-04-02 09:44:28 +0800

    good

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ $( document ).ready(function(){
 									$('#cheng').css('display','initial');
 									$('#cheng').fadeTo('slow', 0.9);
 									setTimeout(function(){
-										$('.name-show').fadeOut(1000).fadeIn(1000).fadeOut(900).fadeIn(900).fadeOut(700).fadeIn(700).fadeOut(500).fadeIn(500).fadeOut(400).fadeIn(400).fadeOut(300).fadeIn(300).fadeOut(300,function(){
+										$('.name-show').fadeOut(700).fadeIn(700).fadeOut(500).fadeIn(500).fadeOut(400).fadeIn(400).fadeOut(300).fadeIn(300).fadeOut(300,function(){
 											$('.cv').fadeIn(1000);
 										});
 									},1000)
